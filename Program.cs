79a20759057f090999deed8cd3161a6ef171131b
Mahavishnu Jneesh <mahavishnu.afonso@sga.pucminas.br>
2023-11-06 10:09:08 -0300

    teste do programa

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia Tijolão");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "modelo 1", imei: "12121212121212", memoria: 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\nSmartphone Iphone");
+Smartphone iphone = new Iphone(numero: "78912", modelo: "modelo 5s", imei: "11222314231243", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatzap");
