eab6721f7bc335c0b9721d2cd9b0711947cda8e9
Mahavishnu Jneesh <mahavishnu.afonso@sga.pucminas.br>
2023-11-06 10:08:21 -0300

    sobreescrevendo o metodo

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,22 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        
+
+        public Iphone(string numero, string modelo, string imei, int memoria) : base (numero, modelo)
+        {
+
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o  {nomeApp} num Iphone");
+            Console.WriteLine("Tomara que a bateria aguente até o fim da instalação.");
+            
+        }
     }
+        
 }
