f86cfb65a0ea797df11d424a38d8868aec780529
Mahavishnu Jneesh <mahavishnu.afonso@sga.pucminas.br>
2023-11-06 10:08:50 -0300

    classe smarphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,30 @@
 namespace DesafioPOO.Models
 {
-    public abstract class Smartphone
+    public abstract class Smartphone 
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
 
-        public Smartphone(string numero)
+        private int Memoria { get; set;}
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+
+            IMEI = imei;
+
+            Memoria = memoria;
+
+        }
+
+        protected Smartphone(string numero, string modelo)
+        {
+            Numero = numero;
+            Modelo = modelo;
         }
 
         public void Ligar()
